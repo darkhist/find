884f4842df1f28da5c35353f04f6e7301cdafcc3
qmsalas <qmsalas@iastate.edu>
2018-10-26 15:46:30 -0500

    🛠 fixes declare department_table_name update employee insert statements update assignments insert statement fix typo

--- GC01/gca01.py
+++ GC01/gca01.py
@@ -11,24 +11,32 @@ c.execute("CREATE TABLE IF NOT EXISTS employee (ID 'INTEGER' PRIMARY KEY AUTOINC
 c.execute("CREATE TABLE IF NOT EXISTS department (ID 'INTEGER' PRIMARY KEY AUTOINCREMENT, DepartmentName 'TEXT', ManagerName 'TEXT')")
 c.execute("CREATE TABLE IF NOT EXISTS assignments (ID 'INTEGER' PRIMARY KEY AUTOINCREMENT,EmployeeID INTEGER, DepartmentID INTEGER, FOREIGN KEY(EmployeeID) REFERENCES employee(ID), FOREIGN KEY(DepartmentID) REFERENCES department(ID))")
 
-c.execute('INSERT INTO {tn}  VALUES ( NULL, {departmentname}, {managername})'.format(tn=department_table_name, departmentname='"Development"', managername='"Dr. Helmer"'))
+department_table_name = "department"
+c.execute('INSERT INTO {tn}  VALUES (NULL, {departmentname}, {managername})'.format(
+    tn=department_table_name, departmentname='"Development"', managername='"Dr. Helmer"'))
 development_id = c.lastrowid
 
 # inserting employees
 table_name = "employee"
-c.execute('INSERT INTO {tn}  VALUES ({v1}, {v2}, {v3})'.format(tn=table_name, v1=null, v2='"Quinn"', v3='"Salas"', v4='"$1"'))
-c.execute('INSERT INTO {tn}  VALUES ({v1}, {v2}, {v3})'.format(tn=table_name, v1=null, v2='"Trace"', v3='"Ohrt"', v4='"$1"'))
-c.execute('INSERT INTO {tn}  VALUES ({v1}, {v2}, {v3})'.format(tn=table_name, v1=null, v2='"Joe"', v3='"Ward"', v4='"$1"'))
-c.execute('INSERT INTO {tn}  VALUES ({v1}, {v2}, {v3})'.format(tn=table_name, v1=null, v2='"Tyler"', v3='"Moul"', v4='"$1"'))
-c.execute('INSERT INTO {tn}  VALUES ({v1}, {v2}, {v3})'.format(tn=table_name, v1=null, v2='"Cole"', v3='""', v4='"$1"'))
+c.execute('INSERT INTO {tn}  VALUES (NULL, {v2}, {v3}, {v4})'.format(
+    tn=table_name, v2='"Quinn"', v3='"Salas"', v4='"$1"'))
+c.execute('INSERT INTO {tn}  VALUES (NULL, {v2}, {v3}, {v4})'.format(
+    tn=table_name, v2='"Trace"', v3='"Ohrt"', v4='"$1"'))
+c.execute('INSERT INTO {tn}  VALUES (NULL, {v2}, {v3}, {v4})'.format(
+    tn=table_name, v2='"Joe"', v3='"Ward"', v4='"$1"'))
+c.execute('INSERT INTO {tn}  VALUES (NULL, {v2}, {v3}, {v4})'.format(
+    tn=table_name, v2='"Tyler"', v3='"Moul"', v4='"$1"'))
+c.execute('INSERT INTO {tn}  VALUES (NULL, {v2}, {v3}, {v4})'.format(
+    tn=table_name, v2='"Cole"', v3='"Goode"', v4='"$1"'))
 
 # inseting assignments data
 table_name = "assignments"
 for i in range(0, 4):
-    c.execute('INSERT INTO {tn}  VALUES ({v1}, {v2}, {v3})'.format(tn=table_name, v1=null, v2=i, v3=development_id))
+    c.execute('INSERT INTO {tn} VALUES (NULL, {v2}, {v3})'.format(
+        tn=table_name, v2=i, v3=development_id))
 
 # grab info for printing
-c.execute("SELECT * FROM deparment, employee, assignments WHERE department.ID = assignments.DepartmentID AND assignments.EmployeeID = employee.ID")
+c.execute("SELECT * FROM department, employee, assignments WHERE department.ID = assignments.DepartmentID AND assignments.EmployeeID = employee.ID")
 
 all_rows = c.fetchall()
 print(all_rows)
