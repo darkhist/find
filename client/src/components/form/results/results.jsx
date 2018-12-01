8554ba25b8db7e4deb78ad7be3fb06a1aea2ade7
darkhist <qmsalas@iastate.edu>
2018-12-01 02:50:49 -0600

    wip: styling results

--- client/src/components/form/results/results.jsx
+++ client/src/components/form/results/results.jsx
@@ -2,7 +2,39 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Subtitle = styled.h3`
+  font-size: 1.5em;
   text-align: center;
+`;
+
+const Card = styled.div`
+  border: 1px solid black;
+  text-align: left;
+  margin: 3em 0;
+`;
+
+const Job = styled.li`
+  font-size: 1em;
+  padding: .5em;
+`;
+
+const Title = styled.h5`
+  font-size: 1em;
+  margin: 0;
+`;
+
+const Company = styled.h4`
+  font-size: 1em;
+  margin: 0;
+`;
+
+const Apply = styled.h6`
+  font-size: 1em;
+  margin: 0;
+`;
+
+const CompanyURL = styled.a`
+  font-weight: bold;
+  text-decoration: none;
 `;
 
 const Results = ({ results }) => {
@@ -24,16 +56,26 @@ const Results = ({ results }) => {
   }
 
   const jobs = results.map(result => (
-    <li key={result.id}>
-      {result.company}
-      <br />
-      {result.title}
-      <br />
-      Apply Here:
-      {result.how_to_apply}
-      <br />
-      {result.company_url}
-    </li>
+    <Card key={result.id}>
+      <Job>
+        <Company>
+          {result.company}
+        </Company>
+
+        <Title>
+          {result.title}
+        </Title>
+
+        <Apply>
+          Apply: &thinsp;
+          {result.how_to_apply}
+        </Apply>
+
+        <CompanyURL>
+          {result.company_url}
+        </CompanyURL>
+      </Job>
+    </Card>
   ));
 
   return (
