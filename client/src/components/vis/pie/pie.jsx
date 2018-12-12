7528c7fe392b03e8e98de96003233a783da88db6
darkhist <qmsalas@iastate.edu>
2018-12-11 18:07:07 -0600

    add chart title chore: update sass

--- client/src/components/vis/pie/pie.jsx
+++ client/src/components/vis/pie/pie.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { RadialChart } from 'react-vis';
+import styled from 'styled-components';
+
+export const ChartTitle = styled.h4`
+  font-size: 1.15em;
+  text-align: center;
+`;
 
 const Pie = ({ data }) => (
-  <RadialChart
-    data={data}
-    width={325}
-    height={325}
-    showLabels
-  />
+  <div className="pie-container">
+    <ChartTitle>
+      Keyword Relevance
+    </ChartTitle>
+
+    <RadialChart
+      data={data}
+      width={325}
+      height={325}
+      showLabels
+    />
+  </div>
 );
 
 export default Pie;
